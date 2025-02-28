b708dcc093e110b26128bdf306b5be7da3e43966
romain decline <83162519+romain-decline@users.noreply.github.com>
2025-02-28 15:11:20 +0100

    push updates

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: "lists#index"
+
   resources :lists do
     resources :bookmarks, only: [:new, :create, :destroy]
   end
